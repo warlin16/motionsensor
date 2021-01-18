d8346840000152cd7a40a8e9b0a367594cd6b76a
Warlin Reyes <warlin16@gmail.com>
2021-01-18 12:31:54 -0500

    add dependencies for connecting to rpi, initialize entry point

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"log"
+	"motionsensor/config"
 	"os"
 	"path/filepath"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
 )
 
 func init() {
@@ -16,4 +21,23 @@ func init() {
 
 func main() {
 	println("Hello, world from the RPI!")
+	c := config.Config{}
+	c.FetchRpiDetails()
+	a := firmata.NewAdaptor(c.Rpi.FirmataPort)
+	s := gpio.NewPIRMotionDriver(a, "5")
+
+	test := func() {
+		s.On(gpio.MotionDetected, func(data interface{}) {
+			println("Motion was detected!")
+		})
+		s.On(gpio.MotionStopped, func(data interface{}) {
+			println("Motion has stopped")
+		})
+	}
+
+	rpi := gobot.NewRobot("motionSensor", []gobot.Connection{a}, []gobot.Device{s}, test)
+
+	if err := rpi.Start(); err != nil {
+		log.Fatalf("Rpi could not start: " + err.Error())
+	}
 }
